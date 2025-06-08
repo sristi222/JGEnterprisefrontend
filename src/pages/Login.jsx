dbad54c4eff0229ae7e4e2bc4705267539b0aa4d
Sristi Shrestha <132660312+sristi222@users.noreply.github.com>
2025-06-08 23:31:34 +0545

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login({ onLogin }) {
     setIsLoading(true)
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://jgenterprisebackend-1.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: username, password })
